841ce530d5a66ff6650d9f8e0b1b052c961ae223
Tiago Viana <tiagovianabatista@gmail.com>
2024-04-21 12:19:01 -0300

    Template teste AppService

--- src/AbpHelper.Core/Templates/Crud/Groups/Test/test/{{ProjectInfo.FullName}}.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/{{EntityInfo.Name}}AppServiceTests.cs
+++ src/AbpHelper.Core/Templates/Crud/Groups/Test/test/{{ProjectInfo.FullName}}.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/{{EntityInfo.Name}}AppServiceTests.cs
@@ -6,41 +6,147 @@ using {{ ProjectInfo.FullName }}.EntityFrameworkCore;
 
 namespace {{ EntityInfo.Namespace }};
 
-[Collection(SISPRECTestConsts.CollectionDefinitionName)]
+{{- 
+entityNameLowerCase = EntityInfo.Name | abp.camel_case
+entityObj1 = entityNameLowerCase + "Obj1"
+entityObj2 = entityNameLowerCase + "Obj2"
+-}}
 public class {{ EntityInfo.Name }}AppServiceTests : BaseAppServiceTests<{{ ProjectInfo.Name }}EntityFrameworkCoreTestModule>
 {
     private readonly I{{ EntityInfo.Name }}AppService _appService;
     private readonly I{{ EntityInfo.Name }}Repository _repository;
+    private readonly {{ EntityInfo.Name }} {{ entityObj1 }};
+    private readonly {{ EntityInfo.Name }} {{ entityObj2 }};
 
     public {{ EntityInfo.Name }}AppServiceTests()
     {
         _appService = GetRequiredService<I{{ EntityInfo.Name }}AppService>();
         _repository = GetRequiredService<I{{ EntityInfo.Name }}Repository>();
-        
-        //Usando repositório, insira registros no BD para usá-los nos testes.
-        var {{ EntityInfo.Name | abp.camel_case }}1 = new {{ EntityInfo.Name }}
+
+        {{ entityObj1 }} = new {{ EntityInfo.Name }}
         {
-            //Coloque valores das propriedades Exemplo:
-            //Id = 10
+            {{~ for prop in EntityInfo.Properties ~}}
+            {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+            {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+            {{~ if EntityInfo.CompositeKeys[0].Name == prop.Name ; continue; end ~}}
+            {{ prop.Name }} = ({{prop.Type}})"",//preencher com valores;
+            {{~ end ~}}
         };
-        _repository.InsertAsync({{ EntityInfo.Name | abp.camel_case }}1, true);
+        _repository.InsertAsync(entityObj1, true);
+        
+        {{ entityObj2 }} = new {{ EntityInfo.Name }}
+        {
+            {{~ for prop in EntityInfo.Properties ~}}
+            {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+            {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+            {{~ if EntityInfo.CompositeKeys[0].Name == prop.Name ; continue; end ~}}
+            {{ prop.Name }} = ({{prop.Type}})"",//preencher com valores;
+            {{~ end ~}}
+        };
+        _repository.InsertAsync(entityObj2, true);
     }
 
-    /*
-     * Exemplo:
     [Fact]
-    public async Task Obter_{{ EntityInfo.Name }}_Por_Id_Deve_Retornar_Corretamente()
+    public async Task Criar_{{ EntityInfo.Name }}_Deve_Passar()
     {
-        //Arrange
-        var id = 10;
+        // Arrange
+        var input = new CreateUpdate{{ EntityInfo.Name }}Dto
+        {
+            {{~ for prop in EntityInfo.Properties ~}}
+            {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+            {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+            {{~ if EntityInfo.CompositeKeys[0].Name == prop.Name ; continue; end ~}}
+            {{ prop.Name }} = ({{prop.Type}})"",//preencher com valores;
+            {{~ end ~}}
+        };
 
-        //Act
-        var resultado = await _appService.GetAsync(id);
+        // Act
+        var result = await _appService.CreateAsync(input);
 
-        //Assert
-        resultado.ShouldNotBeNull();
-        resultado.Id.ShouldBe(id);
+        // Assert
+        result.ShouldNotBeNull();
+        {{~ for prop in EntityInfo.Properties ~}}
+        {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+        {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+        result.{{prop.Name}}.ShouldBe(input.{{prop.Name}});
+        {{~ end ~}}
     }
-    */
+
+    [Fact]
+    public async Task Atualizar_{{ EntityInfo.Name }}_Deve_Passar()
+    {
+        // Arrange
+        var objetoOriginal = _repository.GetListAsync().Result.FirstOrDefault();
+        var input = new CreateUpdate{{ EntityInfo.Name }}Dto
+        {
+            {{~ for prop in EntityInfo.Properties ~}}
+            {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+            {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+            {{~ if EntityInfo.CompositeKeys[0].Name == prop.Name ; continue; end ~}}
+            {{ prop.Name }} = ({{prop.Type}})"",//preencher com valores;
+            {{~ end ~}}
+        };
+
+        // Act
+        var result = await _appService.UpdateAsync(objetoOriginal.{{EntityInfo.CompositeKeys[0].Name}}, input);
+
+        // Assert
+        result.ShouldNotBeNull();
+        {{~ for prop in EntityInfo.Properties ~}}
+        {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+        {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+        result.{{prop.Name}}.ShouldBe(input.{{prop.Name}});
+        {{~ end ~}}
+    }
+
+    [Fact]
+    public async Task Excluir_{{ EntityInfo.Name }}_Deve_Passar()
+    {
+        // Arrange
+        var objetoParaExcluir = await _repository.InsertAsync(new {{ EntityInfo.Name }}
+        {
+            {{~ for prop in EntityInfo.Properties ~}}
+            {{~ if string.contains(prop.Modifiers, "virtual") ; continue; end ~}}
+            {{~ if prop | abp.is_ignore_property ; continue; end ~}}
+            {{~ if EntityInfo.CompositeKeys[0].Name == prop.Name ; continue; end ~}}
+            {{ prop.Name }} = ({{prop.Type}})"",//preencher com valores;
+            {{~ end ~}}
+        }, autoSave: true);
+
+        // Act
+        await _appService.DeleteAsync(objetoParaExcluir.Id);
+
+        // Assert
+        var objetoDeletado = await _repository.FindAsync(a => a.Id == objetoParaExcluir.Id);
+        objetoDeletado.ShouldBeNull();
+    }
+
+    [Fact]
+    public async Task Obter_{{ EntityInfo.Name }}_Por_Id_Deve_Passar()
+    {
+        // Arrange
+        var objetoExistente = await _repository.FindAsync(a => a.{{EntityInfo.CompositeKeys[0].Name}} == {{entityObj1}}.{{EntityInfo.CompositeKeys[0].Name}});
+
+        // Act
+        var result = await _appService.GetAsync(objetoExistente.{{EntityInfo.CompositeKeys[0].Name}});
+
+        // Assert
+        result.ShouldNotBeNull();
+        result.{{EntityInfo.CompositeKeys[0].Name}}.ShouldBe(objetoExistente.{{EntityInfo.CompositeKeys[0].Name}});
+    }
+
+    [Fact]
+    public async Task Obter_{{ EntityInfo.Name }}_Com_Ordenacao_Padrao_Deve_Passar()
+    {
+        // Arrange
+        var input = new {{ EntityInfo.Name }}GetListInput();  // Sem filtros específicos para testar a ordenação padrão.
+
+        // Act
+        var result = await _appService.GetListAsync(input);
+
+        // Assert
+        result.Items.ShouldNotBeEmpty(); // Correção aplicada diretamente ao acessar a propriedade Items
+        result.Items.OrderBy(l => l.{{EntityInfo.CompositeKeys[0].Name}}).SequenceEqual(result.Items).ShouldBeTrue(); // Corrigido para usar OrderBy e SequenceEqual para verificar a ordem
+    }
 }
 
